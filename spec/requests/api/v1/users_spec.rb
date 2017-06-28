3c2fb20f50925eb50b786b8823a420516e18e44c
Danilo <danilocordeiro.ti@gmail.com>
2017-06-28 16:50:11 -0300

    Adding update method

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -65,4 +65,36 @@ RSpec.describe 'Users API', type: :request do
     end
   end
 
+  describe 'PUT /users/:id' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      put "/users/#{user_id}", params: { user: user_params }, headers: headers
+    end
+
+    context 'when the resquest params are valid' do
+      let(:user_params) { { email: 'new_email@tackmanager.com' } }
+
+      it 'return status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'return json data for updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
+
+      it 'return status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'return json data for the errors' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+    end
+  end
 end
