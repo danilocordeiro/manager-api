5487a56ec9de1cfc09970756394fb70f99afee37
Danilo <danilocordeiro.ti@gmail.com>
2017-07-01 07:12:37 -0300

    Adding auth_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_uniqueness_of :auth_token
+
+  def info
+    "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value('danilo@gmail.com').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe '#info' do
+    it 'returns email, created_at and Token' do
+      user.save!
+      allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+
+      expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: #{Devise.friendly_token}")
+    end
+  end
 end
